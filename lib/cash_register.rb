4e650eefd3f8cf4ae3b48a5d99dc587103d22ec8
Trish Ogen <patriciaogen@gmail.com>
2019-05-29 02:52:50 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,13 +1,11 @@
 class CashRegister
-  attr_reader :discount, :total
+  attr_reader :discount
+  attr_accessor :total
 
   def initialize(discount=0)
     @total = 0
     @discount = discount
   end
 
-  def total
-    self.total
-  end
 
 end
