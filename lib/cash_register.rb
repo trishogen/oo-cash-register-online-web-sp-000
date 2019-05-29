cb56c030ad2462cdc5000c5f5535f1ae09585fb8
Trish Ogen <patriciaogen@gmail.com>
2019-05-29 03:09:33 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,7 +9,7 @@ class CashRegister
   end
 
   def add_item(item, price, quantity=1)
-    self.items << [item] * quantity
+    self.items += [item] * quantity
     self.total += price * quantity
   end
 
